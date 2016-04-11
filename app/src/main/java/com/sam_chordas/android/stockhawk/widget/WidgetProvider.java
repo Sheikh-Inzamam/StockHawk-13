6e02b1be9ec987420593ffa129c1a5d8d04435a5
debun8 <debun8@users.noreply.github.com>
2016-04-11 13:44:12 -0700

    added widget ContentObserver

--- app/src/main/java/com/sam_chordas/android/stockhawk/widget/WidgetProvider.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/WidgetProvider.java
@@ -4,13 +4,18 @@ import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.ContentObserver;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.util.Log;
 import android.widget.RemoteViews;
 
 import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.data.QuoteProvider;
 
 
 public class WidgetProvider extends AppWidgetProvider {
@@ -18,21 +23,65 @@ public class WidgetProvider extends AppWidgetProvider {
     private static final String TAG = WidgetProvider.class.getSimpleName();
     public static String REFRESH_ACTION = "refresh";
 
+    private static HandlerThread sWorkerThread;
+    private static Handler sWorkerQueue;
+    private static WidgetDataProviderObserver sDataObserver;
+
+    public WidgetProvider() {
+        Log.d(TAG, "WidgetProvider()");
+        // Start the worker thread
+        sWorkerThread = new HandlerThread("WidgetProvider-worker");
+        sWorkerThread.start();
+        sWorkerQueue = new Handler(sWorkerThread.getLooper());
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Register for external updates to the data to trigger an update of the widget.  When using
+        // content providers, the data is often updated via a background service, or in response to
+        // user interaction in the main app.  To ensure that the widget always reflects the current
+        // state of the data, we must listen for changes and update ourselves accordingly.
+        Log.d(TAG, "onEnabled");
+        final ContentResolver r = context.getContentResolver();
+        if (sDataObserver == null) {
+            final AppWidgetManager mgr = AppWidgetManager.getInstance(context);
+            final ComponentName cn = new ComponentName(context, WidgetProvider.class);
+            sDataObserver = new WidgetDataProviderObserver(mgr, cn, sWorkerQueue);
+            r.registerContentObserver(QuoteProvider.Quotes.CONTENT_URI, true, sDataObserver);
+        }
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+        final ContentResolver r = context.getContentResolver();
+        r.unregisterContentObserver(sDataObserver);
+        if (sWorkerThread != null) {
+            sWorkerThread.quit();
+        }
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(REFRESH_ACTION)) {
-            final AppWidgetManager mgr = AppWidgetManager.getInstance(context);
-            final ComponentName cn = new ComponentName(context, WidgetProvider.class);
-            int[] appWidgetIds = mgr.getAppWidgetIds(cn);
-            mgr.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_listview);
+            final Context ctx = context;
+            sWorkerQueue.removeMessages(0);
+            sWorkerQueue.post(new Runnable() {
+                @Override
+                public void run() {
+                    final AppWidgetManager mgr = AppWidgetManager.getInstance(ctx);
+                    final ComponentName cn = new ComponentName(ctx, WidgetProvider.class);
+                    int[] appWidgetIds = mgr.getAppWidgetIds(cn);
+                    mgr.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_listview);
+                }
+            });
+
         }
         super.onReceive(context, intent);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        Log.d(TAG, "onUpdate");
-
         for (int widgetId:appWidgetIds) {
             // Set up listview adapter
             final Intent intent = new Intent(context, WidgetService.class);
@@ -51,6 +100,30 @@ public class WidgetProvider extends AppWidgetProvider {
             appWidgetManager.updateAppWidget(widgetId, layout);
         }
         super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+
+    /**
+     * Our data observer just notifies an update for all weather widgets when it detects a change.
+     */
+    // credit: https://android.googlesource.com/platform/development/+/master/samples/WeatherListWidget/src/com/example/android/weatherlistwidget/WeatherWidgetProvider.java
+    class WidgetDataProviderObserver extends ContentObserver {
+        private AppWidgetManager mAppWidgetManager;
+        private ComponentName mComponentName;
+
+        WidgetDataProviderObserver(AppWidgetManager mgr, ComponentName cn, Handler h) {
+            super(h);
+            mAppWidgetManager = mgr;
+            mComponentName = cn;
+        }
+        @Override
+        public void onChange(boolean selfChange) {
+            // The data has changed, so notify the widget that the collection view needs to be updated.
+            // In response, the factory's onDataSetChanged() will be called which will requery the
+            // cursor for the new data.
+            mAppWidgetManager.notifyAppWidgetViewDataChanged(
+                    mAppWidgetManager.getAppWidgetIds(mComponentName), R.id.widget_listview);
+        }
     }
 }
 
