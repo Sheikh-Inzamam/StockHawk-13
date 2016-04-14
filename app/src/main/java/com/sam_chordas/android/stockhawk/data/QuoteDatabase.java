3dc78981e7c2d421de9610c3811e2eb8ba109fe4
debun8 <debun8@users.noreply.github.com>
2016-04-14 10:41:46 -0700

    removed quote db onupgrade

--- app/src/main/java/com/sam_chordas/android/stockhawk/data/QuoteDatabase.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/data/QuoteDatabase.java
@@ -1,12 +1,6 @@
 package com.sam_chordas.android.stockhawk.data;
 
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
-
 import net.simonvt.schematic.annotation.Database;
-import net.simonvt.schematic.annotation.OnCreate;
-import net.simonvt.schematic.annotation.OnUpgrade;
 import net.simonvt.schematic.annotation.Table;
 
 /**
@@ -23,37 +17,4 @@ public class QuoteDatabase {
 
     @Table(QuoteColumns.class)
     public static final String QUOTES = "quotes";
-
-/*  here is generated code for onCreate
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL(QUOTES);
-        com.sam_chordas.android.stockhawk.data.QuoteDatabase.onCreate(context, db);
-    }
-*/
-
-    @OnCreate
-    public static void onCreate(Context context, SQLiteDatabase db) {
-    }
-
-    // credit: https://github.com/SimonVT/schematic/issues/12
-    @OnUpgrade
-    public static void onUpgrade(Context context, SQLiteDatabase db, int oldVersion, int newVersion) {
-        Log.d(TAG, "onUpgrade");
-        final String UPGRADE_SCRIPT = "DROP TABLE IF EXISTS " + "quotes"; // cant use QUOTES gets generated into create
-
-        if (oldVersion < newVersion) {
-            db.beginTransaction();
-            try {
-                db.execSQL(UPGRADE_SCRIPT);
-                // todo call onCreate here, how?
-                QuoteDatabase.onCreate(context, db);
-                db.setTransactionSuccessful();
-
-            } catch (Exception e) {
-                Log.e(QuoteDatabase.class.getSimpleName(), String.format(
-                        "Failed to upgrade database with script: %s", UPGRADE_SCRIPT), e);
-            }
-        }
-    }
 }
